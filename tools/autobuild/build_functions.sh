0fc64dfbdfc1c965abad2a42a87fef0343a018c4
Timofey <t.danshin@corp.mail.ru>
2016-12-06 18:14:25 +0300

    No progress yet

--- tools/autobuild/build_functions.sh
+++ tools/autobuild/build_functions.sh
@@ -28,14 +28,32 @@ BuildCmake() {
 
     echo ">>>>> CC is $CC"
 
-    CC=$CC CXX=$CXX cmake \
-     "-DCMAKE_SYSTEM_NAME=Android" \
-     "-DCMAKE_ANDROID_ARCH_ABI=$ARCH" \
-    "-DANDROID_NDK=/Users/Shared/android-ndk-r12b/" \
-    "-DINCLUDE_DIRECTORIES=/Users/Shared/android-ndk-r12b/sources/cxx-stl/llvm-libc++" \
-    "-DCMAKE_CXX_COMPILER=$CXX" \
-    "-DCMAKE_C_COMPILER=$CC" \
-     "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DNO_TESTS=TRUE" "-DPLATFORM=$PLATFORM" "$MY_PATH/../.." \
+    cmake="/Users/Shared/AndroidSdk/cmake/3.6.3155560/bin/cmake"
+
+#Error while executing '/Users/Shared/AndroidSdk/cmake/3.6.3155560/bin/cmake' with arguments {-H/Users/t.danshin/Documents/projects/omim -B/Users/t.danshin/Documents/projects/omim/android/.externalNativeBuild/cmake/preinstallBeta/armeabi -GAndroid Gradle - Ninja -DANDROID_ABI=armeabi -DANDROID_NDK=/Users/Shared/android-ndk-r12b -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=/Users/t.danshin/Documents/projects/omim/android/build/intermediates/cmake/preinstall/beta/obj/armeabi -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=/Users/Shared/AndroidSdk/cmake/3.6.3155560/bin/ninja -DCMAKE_TOOLCHAIN_FILE=/Users/Shared/AndroidSdk/cmake/3.6.3155560/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=15}
+
+#    "-B/Users/t.danshin/Documents/projects/omim/android/.externalNativeBuild/cmake/preinstallBeta/armeabi" \
+#    "-H/Users/t.danshin/Documents/projects/omim" \
+
+    #CC=$CC CXX=$CXX
+    $cmake \
+    "-GAndroid Gradle - Ninja" \
+    "-DANDROID_ABI=armeabi" \
+    "-DANDROID_NDK=/Users/Shared/android-ndk-r12b" \
+    "-DCMAKE_BUILD_TYPE=Release" \
+    "-DCMAKE_MAKE_PROGRAM=/Users/Shared/AndroidSdk/cmake/3.6.3155560/bin/ninja" \
+    "-DCMAKE_TOOLCHAIN_FILE=/Users/Shared/AndroidSdk/cmake/3.6.3155560/android.toolchain.cmake" \
+    "-DANDROID_NATIVE_API_LEVEL=15"\
+    "-DPLATFORM=$PLATFORM" "$MY_PATH/../.."
+
+#    "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=/Users/t.danshin/Documents/projects/omim/android/build/intermediates/cmake/preinstall/beta/obj/armeabi" \
+#     "-DCMAKE_SYSTEM_NAME=Android" \
+#     "-DCMAKE_ANDROID_ARCH_ABI=$ARCH" \
+#    "-DANDROID_NDK=/Users/Shared/android-ndk-r12b/" \
+#    "-DINCLUDE_DIRECTORIES=/Users/Shared/android-ndk-r12b/sources/cxx-stl/llvm-libc++" \
+#    "-DCMAKE_CXX_COMPILER=$CXX" \
+#    "-DCMAKE_C_COMPILER=$CC" \
+#     "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DNO_TESTS=TRUE"  \
 #    CC=$CC CXX=$CXX cmake "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" -DCMAKE_OSX_ARCHITECTURES=$ARCH -DNO_TESTS=TRUE -DPLATFORM=$PLATFORM "$MY_PATH/../.."
 #    make clean > /dev/null || true
     make -j \
